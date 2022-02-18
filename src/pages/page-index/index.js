965c4c3d52e7ae542fd32fffa69f36d56e511c8d
guyang66 <13588295865@163.com>
2022-02-18 15:53:49 +0800

    一些插件库引入，便于开发：jqeury、swiper3.0 vediojs

--- src/pages/page-index/index.js
+++ src/pages/page-index/index.js
@@ -8,6 +8,17 @@ const bannerData = {
 }
 
 const tabsData = [{
+  name: 'tabs1',
+  anchor: 'tabs1',
+},{
+  name: 'tabs2',
+  anchor: 'tabs2',
+},{
+  name: 'tabs3',
+  anchor: 'tabs3',
+},{
+  name: 'tabs4',
+  anchor: 'tabs5',
 }];
 
 const container = template();
--- src/pages/page-index/main.js
+++ src/pages/page-index/main.js
@@ -1,1 +1,10 @@
 import './style';
+import Swiper from 'swiper';
+$(function() {
+  var bannerSwiper = new Swiper('.swiper-container', {
+    autoplay : 3000,
+    speed: 500,
+    loop: true,
+    mousewheelControl : true,
+  });
+})
