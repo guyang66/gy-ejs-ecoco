69bc40e1319fd2b74ed7582b40cb65f0936e3540
guyang66 <13588295865@163.com>
2022-04-06 13:57:12 +0800

    tmp

--- src/page/about/page-joinus/index.js
+++ src/page/about/page-joinus/index.js
@@ -2,14 +2,19 @@ import template from './template';
 import htmlRender from '@/layout/index';
 import form from '@/component/form'
 import env from './module.env'
+import welfare from './module.welfare'
+import resume from './module.resume'
 
 import bannerData from "@/data/about/joinus/banner";
 import envData from "@/data/about/joinus/env";
+import welfareData from "@/data/about/joinus/welfare";
+import resumeData from "@/data/about/joinus/resume";
+
 const tabsData = [{
   name: '工作环境',
   anchor: 'env',
 },{
-  name: '工作福利',
+  name: '福利待遇',
   anchor: 'welfare',
 },{
   name: '简历投递',
@@ -18,7 +23,10 @@ const tabsData = [{
 
 const formView = form()
 const envView = env({envData})
-const container = template({formView, envView});
+const welfareView = welfare({welfareData})
+const resumeView = resume({resumeData})
+
+const container = template({formView, envView, welfareView, resumeView});
 export default htmlRender({
   key: 'joinus',
   navKey: 'about', // 用于导航栏item active判断，即属于哪一个导航栏模块
