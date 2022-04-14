9ab605ecd45fecc549d8b2242380f9a1f0915a12
guyang66 <13588295865@163.com>
2022-04-14 13:40:15 +0800

    表单临时

---
+++ src/data/form/logo.js
@@ -0,0 +1,21 @@
+import a1 from '@/assets/images/logo/other/a2.svg'
+import a2 from '@/assets/images/logo/other/b2.svg'
+import a3 from '@/assets/images/logo/other/c2.svg'
+import a4 from '@/assets/images/logo/other/d2.svg'
+import a5 from '@/assets/images/logo/other/e2.svg'
+import a6 from '@/assets/images/logo/other/f2.svg'
+import a7 from '@/assets/images/logo/other/g2.svg'
+
+const data = [
+  a1,
+  a2,
+  a3,
+  a4,
+  a5,
+  a6,
+  a7,
+  a2,
+  a5,
+  a1,
+]
+export default data
